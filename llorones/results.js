d2d50509bb71ac433230cb0318994dbb4823b9c2
josecrack00 <josecrack00@gmail.com>
2026-06-04 21:48:49 -0500

    Add files via upload

---
+++ llorones/results.js
@@ -0,0 +1,88 @@
+const RESULTS = {
+  groups: {},
+  thirdPlace: [],
+  groupMatches: {},
+
+  knockout: {
+    round32: [],
+    round16: [],
+    quarterfinals: [],
+    semifinals: [],
+
+    champion: "",
+    runnerUp: "",
+    finalists: [],
+
+    thirdPlaceWinner: "",
+    final: "",
+    thirdPlace: "",
+
+    matches: {
+      round32: [
+        // {
+        //   match: 73,
+        //   team1: "",
+        //   team2: "",
+        //   winner: ""
+        // }
+      ],
+
+      round16: [
+        // {
+        //   match: 89,
+        //   team1: "",
+        //   team2: "",
+        //   winner: ""
+        // }
+      ],
+
+      quarterfinals: [
+        // {
+        //   match: 97,
+        //   team1: "",
+        //   team2: "",
+        //   winner: ""
+        // }
+      ],
+
+      semifinals: [
+        // {
+        //   match: 101,
+        //   team1: "",
+        //   team2: "",
+        //   winner: ""
+        // }
+      ],
+
+      thirdPlace: [
+        // {
+        //   match: 103,
+        //   team1: "",
+        //   team2: "",
+        //   winner: ""
+        // }
+      ],
+
+      final: [
+        // {
+        //   match: 104,
+        //   team1: "",
+        //   team2: "",
+        //   winner: ""
+        // }
+      ]
+    }
+  },
+
+  semifinalists: [],
+  finalists: [],
+
+  champion: "",
+  runnerUp: "",
+  thirdPlaceWinner: "",
+
+  awards: {
+    goldenBoot: [],
+    goldenBall: []
+  }
+};

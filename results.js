d56f02907ba448dae909714d07d684349a61274e
josecrack00 <josecrack00@gmail.com>
2026-06-13 01:36:07 -0500

    Update results.js

--- results.js
+++ results.js
@@ -4,6 +4,14 @@ const RESULTS = {
   groupMatches: { "A": {
       "Mexico__South Africa": { "home": 2, "away": 0 },
       "South Korea__Czech Republic": { "home": 2, "away": 1 }
+    }
+    "B": {
+      "Canada__Bosnia & Herzegovina": { "home": 1, "away": 1 }
+    },
+    "C": {
+    },
+    "D": {
+      "Paraguay__USA": { "home": 1, "away": 4 }
     }},
 
   knockout: {
